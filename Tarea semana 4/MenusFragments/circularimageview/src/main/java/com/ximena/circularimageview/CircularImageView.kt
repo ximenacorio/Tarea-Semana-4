4d8739ae9d5d2c14150968ea97dd866f30df6aaf
ximenacorio <71667032+ximenacorio@users.noreply.github.com>
2020-10-09 11:15:07 -0300

    Update CircularImageView.kt

--- Tarea semana 4/MenusFragments/circularimageview/src/main/java/com/ximena/circularimageview/CircularImageView.kt
+++ Tarea semana 4/MenusFragments/circularimageview/src/main/java/com/ximena/circularimageview/CircularImageView.kt
@@ -15,7 +15,6 @@ import kotlin.math.min
 import kotlin.math.roundToInt
 
 /**
- * Copyright (C) 2020 Mikhael LOPEZ
  * Licensed under the Apache License Version 2.0
  */
 class CircularImageView @JvmOverloads constructor(
